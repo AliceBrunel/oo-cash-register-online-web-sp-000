821062d034e430aec71e16851ea39d487623af1d
Alice Brunel <alc.brunel@gmail.com>
2019-07-24 21:27:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  
+  attr_accessor :total
+  
+  @@items = []
+  
+  def initialize
+    @total = 0
+    
+  end
+  
+  def total 
+    self.total
+  end
+  
+  def add_item(title,price)
+    @total += price
+    
+  end
+  
+  def apply_discount
+  
+  end
 
+  def items
+    @@items
+  end
+
+  def void_last_transaction
+    
+  end
+  
+end
