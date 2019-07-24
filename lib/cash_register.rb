8558af5406cb12a154167fab2d038395723a4e34
Alice Brunel <alc.brunel@gmail.com>
2019-07-24 22:23:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @items << title*quantity
+    @items << title
     @last_transaction = @total
     @total += (price * quantity)    
   end
